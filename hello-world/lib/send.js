093dd3c1687ce2d52deadf15fd10064e0bc817b2
sergio.domingues <sergio.domingues@jscrambler.com>
2020-01-09 09:35:57 +0000

    add work-queue examples

--- hello-world/lib/send.js
+++ hello-world/lib/send.js
@@ -1,30 +1,31 @@
 #!/usr/bin/env node
 
-/*eslint no-undef: "error"*/
+/*eslint no-undef: "warn"*/
+/*global Buffer, process*/
 
 var amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', function(error0, connection) {
+amqp.connect('amqp://localhost', function (error0, connection) {
   if (error0) {
     throw error0;
   }
-  connection.createChannel(function(error1, channel) {
+  connection.createChannel(function (error1, channel) {
     if (error1) {
       throw error1;
     }
+
     var queue = 'hello';
-    var msg = 'Hello world';
+    var msg = 'Hello World!';
 
     channel.assertQueue(queue, {
       durable: false
     });
-
     channel.sendToQueue(queue, Buffer.from(msg));
+
     console.log(" [x] Sent %s", msg);
   });
-  
-  setTimeout(function() { 
-    connection.close(); 
-    process.exit(0) 
-    }, 500);
+  setTimeout(function () {
+    connection.close();
+    process.exit(0);
+  }, 500);
 });
--- work-queues/lib/receive.js
+++ work-queues/lib/receive.js
@@ -11,18 +11,23 @@ amqp.connect('amqp://localhost', function (error0, connection) {
       throw error1;
     }
 
-    var queue = 'hello';
+    var queue = 'task_queue';
 
+    // This makes sure the queue is declared before attempting to consume from it
     channel.assertQueue(queue, {
-      durable: false
+      durable: true
     });
 
-    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
-
     channel.consume(queue, function (msg) {
+      var secs = msg.content.toString().split('.').length - 1;
+
       console.log(" [x] Received %s", msg.content.toString());
+      setTimeout(function () {
+        console.log(" [x] Done");
+      }, secs * 1000);
     }, {
-      noAck: true
+      // inform producer with job & consumer status
+      noAck: false
     });
   });
 });
